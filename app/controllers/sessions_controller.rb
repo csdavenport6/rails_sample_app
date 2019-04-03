fecef8d34c02e0c276ca78e446a5f2ea20e39571
Connor Davenport <cdavenport626@gmail.com>
2019-04-02 18:52:43 -0700

    Handle invalid submissions to user login page; add user login integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in and redirect to the users show page
+    else
+      flash.now[:danger] = "Invalid email/password combination" # not quite correct first attempt at error message
+      render 'new'
+    end
+    
+
+  end
+
+  def destroy
+  end
+
+
+
 end
