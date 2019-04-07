198d9e0c21a455776cc8aeaf2ec4239de3549881
Connor Davenport <cdavenport626@gmail.com>
2019-04-07 11:43:34 -0700

    Implemented remember user functionality with permanent cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,14 @@ module SessionsHelper
 
   # returns the current logged-in user, if there is one
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -20,5 +26,12 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
+  # remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
 end
