fd0ff124b3be1e04b74c71ad166fd038d6a62165
Connor Davenport <connor.s.davenport@ey.com>
2019-04-16 06:36:27 -0700

    Add friendly forwarding behavior to user edit pages

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,5 +47,16 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # redirects to stored location, or the default
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 
 end
